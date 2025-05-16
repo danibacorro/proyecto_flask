d1543c0bfe1d83f766cbdd7636ccd6ae1c72e20f
danibacorro <danibacorro@gmail.com>
2025-05-17 01:03:39 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import json
+
 app = Flask(__name__)	
+
+with open('pruebas_coches.json', encoding='utf-8') as f:
+    coches_json = json.load(f)
 
 
 @app.route('/')
 def inicio():
     return render_template("index.html")
+
+@app.route("/busqueda")
+def busqueda():
+    return render_template("busqueda.html")
+
+@app.route('/lista', methods=['POST'])
+def lista():
+    listado = []
+    nombre = request.form.get('buscador', '').lower()
+    for coche in coches_json:
+        for prueba in coche["pruebas"]:
+            if nombre in prueba["modelo"].lower() or nombre == '':
+                listado.append({
+                    "id": prueba["id"],
+                    "modelo": prueba["modelo"],
+                    "consumo_combinado": prueba["consumo"]["combinado"]
+                })
+    return render_template('lista.html', listado=listado)
 
 app.run("0.0.0.0",5000,debug=True)
