8d3acb64cc7364796eed8a2afce2c818a5374c32
danibacorro <danibacorro@gmail.com>
2025-05-17 13:22:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -30,4 +30,4 @@ def lista():
     return render_template('lista.html', listado=listado)
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
